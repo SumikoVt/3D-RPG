1ec970cf8b8d443460e590ee98c818d3aeada6e3
SumikoVt <66512882+SumikoVt@users.noreply.github.com>
2020-08-29 18:05:25 +0800

    20200829

---
+++ RPG/Assets/Scripts/NPC.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話區塊")]
+    public GameObject panelDialog;
+    [Header("名稱")]
+    public Text textName;
+    [Header("內容")]
+    public Text textContent;
+    [Header("打字速度"), Range(0.01f, 1)]
+    public float printSpeed = 0.2f;
+    [Header("打字音效")]
+    public AudioClip soundPrint;
+
+    private AudioSource aud;
+
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public void Dialog()
+    {
+        panelDialog.SetActive(true);
+        textName.text = name;
+        StartCoroutine(Print());
+    }
+
+    /// <summary>
+    /// 打字效果
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Print()
+    {
+        string dialog = data.dialogs[0];                 // 對話 = NPC 資料,對話第一段
+        textContent.text = "";
+
+        for (int i = 0; i < dialog.Length; i++)          // 跑對話第一個字到最後一個字
+        {
+            textContent.text += dialog[i];               // 對話內容,文字 += 對話[]
+            aud.PlayOneShot(soundPrint, 0.2f);
+            yield return new WaitForSeconds(printSpeed);
+        }
+    }
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "玩家") Dialog();
+    }
+}
